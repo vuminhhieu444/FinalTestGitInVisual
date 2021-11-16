3c0942ff1d0c7d9512feb54919cc3e58aab698e0
HieuVu <vuminhhieu444@gmail.com>
2021-11-16 10:47:50 +0700

    Change model

---
+++ FinalTestgGitInVisual/FinalTestgGitInVisual/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalTestgGitInVisual.Model
+{
+    public class Person
+    {
+        int leg;
+        int hand;
+        string name;
+        string age;
+
+        public int Leg { get => leg; set => leg = value; }
+        public int Hand { get => hand; set => hand = value; }
+        public string Name { get => name; set => name = value; }
+        public string Age { get => age; set => age = value; }
+    }
+}
